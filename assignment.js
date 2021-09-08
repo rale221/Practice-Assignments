833befd8e2ebff3e25354a05b4066864c1c1fdc6
Rastko <rastkodjurica@yahoo.com>
2021-09-08 12:08:55 -0400

    assigment finish 1

--- assignment.js
+++ assignment.js
@@ -1,1 +1,21 @@
 const task3Element = document.getElementById('task-3');
+
+function first(){
+      alert('hello')
+}
+
+function second(name){
+     alert(name)
+}
+
+first();
+second('max');
+
+task3Element.addEventListener('click', first)
+
+function third(name, lastName, date){
+     let combined = `${name} ${lastName} ${date}`
+     return combined
+}
+let combinedString = third('one', 'two', 'three')
+alert(combinedString)
